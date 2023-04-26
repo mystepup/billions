8f2f31b3b428cfd3dd504674f54cc7cd6a8c1752
gun bos <gunbos1031@gmail.com>
2023-04-26 23:59:33 +0000

    "Initial commit"

--- pages/api/persons/index.tsx
+++ pages/api/persons/index.tsx
@@ -1,17 +1,19 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
 
 interface Billions {
-    id: string
-    name: string
-    squareImage: string
-    netWorth: number
-    industries: string[]
+  id: string
+  name: string
+  squareImage: string
+  netWorth: number
+  industries: string[]
 }
 
-async function handler(req: NextApiRequest, res:NextApiResponse) {
-    const { data } = await axios.get<Billions[]>('https://billions-api.nomadcoders.workers.dev')
-    return res.json({ ok: true, billions: data })
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { data } = await axios.get<Billions[]>(
+    'https://billions-api.nomadcoders.workers.dev'
+  )
+  return res.json({ ok: true, billions: data })
 }
 
-export default handler;
+export default handler
